31fb280bf7e01c9d6c387f19ad4c7c3540cc062c
kagehisa4 <kagehisa@proton.me>
2026-05-29 17:24:30 +0530

    code to tokenize and vectorize the emails_data

---
+++ vectorizer_tokenizer.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import  re
+vocab = {'refund', 'document' , 'due' , 'reminder', 'cancel' ,
+ 'code', 'password', 'today', 'delivery', 'cut', 'invitation', 'launch',
+ 'free', 'ordered', 'hir', 'ship', 'update', 'review', 'save', '%'}
+ #20 words vocab
+
+vocab = sorted(list(vocab))
+
+token_ = {}
+df = pd.read_csv('annoated_9.csv')
+
+
+def tokenize_and_vectorize(subject):
+    vector = []
+    token = []
+    subject = subject.lower()
+    subject = re.sub(r'[^%a-zA-Z0-9]', '', subject)
+    for word in vocab:
+        if str(word) in subject:
+            token.append(word)
+    for word in vocab:
+        if word in token:
+            vector.append(1)
+        else:
+            vector.append(-1)
+
+    return token, vector
+
+all_tokens = []
+all_vectors = []
+
+for subject in df['subject']:
+    token, vector = tokenize_and_vectorize(subject)
+    all_tokens.append(token)
+    all_vectors.append(vector)
+
+new_df = df
+
+new_df['token'] = all_tokens
+new_df['vector'] = all_vectors
+
+new_df.to_csv('vt.csv')
