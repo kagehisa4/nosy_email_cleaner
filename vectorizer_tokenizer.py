a770663d5a7bc70f6ed6514d1302a327c4220b80
kagehisa4 <kagehisa@proton.me>
2026-06-09 17:40:49 +0530

    add vec torize tokenize

--- vectorizer_tokenizer.py
+++ vectorizer_tokenizer.py
@@ -1,40 +1,63 @@
 import pandas as pd
 import  re
-vocab = ['%', 'cancel', 'code', 'cut', 'delivery', 'document', 'due', 'free', 'hir',
-'invitation', 'launch', 'ordered', 'password', 'refund', 'reminder',
-'review', 'save', 'ship', 'today', 'update']
+vocab = ['noreply', '%', 'cancel', 'connect','connections' 'cut', 'delivery', 'document', 'fee', 'free',
+'invitation', 'impressions', 'launch', 'linkedin' 'noticed', 'ordered','profile', 'recently', 'reminder',
+'review', 'save','searches', 'ship', 'today', 'tomorrow', 'update', 'views'] # problem with plurals
 
 token_ = {}
-df = pd.read_csv('test_data_raw.csv')
+df = pd.read_csv('test_data.csv')
 
 
-def tokenize_and_vectorize(subject):
+def tokenize_and_vectorize(subject,sender):
     vector = []
     token = []
-    subject = subject.lower()
-    subject = re.sub(r'[^%a-zA-Z0-9]', '', subject)
+    subject = re.sub(r'[^a-zA-Z0-9 %]', '', subject)
+    sender = re.sub(r'[^a-zA-Z0-9]', '', sender)
+    #print('a:  ', subject)
+    list = subject.lower().split() # a list
+    if 'noreply' in sender.lower():
+        list.append('noreply')
+
+    #print(list)
     for word in vocab:
-        if str(word) in subject:
+        #print(type(word))
+        if word in list:
             token.append(word)
-    for word in vocab:
-        if word in token:
             vector.append(1)
         else:
             vector.append(-1)
 
-    return token, vector
+
+    return token, vector, subject
 
 all_tokens = []
 all_vectors = []
+all_subjects = []
+new_df = df
 
-for subject in df['subject']:
-    token, vector = tokenize_and_vectorize(subject)
+for i in range(len(df)):
+    old_subject = df['subject'].iloc[i]
+    sender = df['sender'].iloc[i]
+    #print('b:',  subject)
+    token, vector, new_subject = tokenize_and_vectorize(old_subject, sender)
     all_tokens.append(token)
     all_vectors.append(vector)
+    all_subjects.append(new_subject)
+    # all_vectors.append(vector)
+
+new_df['modified subject'] = all_subjects
+new_df['modified token'] = all_tokens
+new_df['modified vector'] = all_vectors
+
+new_df.to_csv('modified_4.csv')
+
+'''for i in range(len(all_tokens)):
+    print('subject: ', all_subject[i],
+        'tokens:' , all_tokens[i], 'vectors: ', all_vectors[i])
 
 new_df = df
 
-new_df['token'] = all_tokens
-new_df['vector'] = all_vectors
+new_df['token_new'] = all_tokens
+# new_df['vector'] = all_vectors
 
-new_df.to_csv('test_data.csv')
+new_df.to_csv('new_data.csv')'''
